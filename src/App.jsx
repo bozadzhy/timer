8a05e411f108688e9d5f0d16ac6a5c7fbba15380
FedorBoz <bozadzhy.f@gmail.com>
2024-07-30 14:39:05 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,16 @@ const Timer = () => {
         <div className="rest">time to rest and do some exercise</div>
       )}
 
+      <img
+        className="photo"
+        src="https://img.freepik.com/free-photo/bluish-plant-leaves-textured-background_53876-107989.jpg?w=996&t=st=1722338557~exp=1722339157~hmac=c083cf4aa445020574e861f69d0deef3e7e2db183e02f5d6fff0f249ff88e4dd"
+        alt="foto"
+      />
+
       <div className="container">
         <div>
           <div>Today {today}</div>
-          {hour < 10 ? "0" + hour : hour}:
-          {minute < 10 ? "0" + minute : minute}
+          {hour < 10 ? "0" + hour : hour}:{minute < 10 ? "0" + minute : minute}
         </div>
         <p>
           press <button className="btn">start</button> when you start working
